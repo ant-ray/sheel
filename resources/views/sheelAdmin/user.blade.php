d45a33b6b563905081fb76e711ef394b1b9ef64e
やなぎさわ <y.y.baske.74c@gmail.com>
2022-11-01 14:54:59 +0900

    従業員詳細画面更新

--- resources/views/sheelAdmin/user.blade.php
+++ resources/views/sheelAdmin/user.blade.php
@@ -11,13 +11,13 @@
                     <div class="tel">連絡先{{ $user->tel }}</div>
                     <div class="delete">
                         <script type="text/javascript">
-                            function disp(){
+                            function disp() {
                                 // 「OK」時の処理開始 ＋ 確認ダイアログの表示
-                                if(window.confirm('本当に実行しますか？')){
-                                    location.href='{{ route('userDelete') }}?id={{ $user->id }}'
+                                if (window.confirm('本当に実行しますか？')) {
+                                    location.href = '{{ route('userDelete') }}?id={{ $user->id }}'
                                 }
                             }
-                            </script>
+                        </script>
                         <button class="backButton" onclick="disp()">削除</button>
                     </div>
                 </div>
@@ -26,7 +26,11 @@
                         <p>従業員ID: 『{{ $user->id }}』</p>
                         <p>メールアドレス:『{{ $user->email }}』</p>
                         <!--- もし管理者だった場合は管理者を返す ---->
+                        @isset($institutionName->name)
                             <p>就労施設名:『{{ $institutionName->name }}』</p>
+                        @else
+                            <p>『管理者』</p>
+                        @endisset
                     </div>
                     <div class="back">
                         <button class="backButton" onClick="history.back();">戻る</button>
